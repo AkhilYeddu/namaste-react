13a429795c64029f7a2aba80678d63b42d3c12ce
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-07 01:08:59 +0530

    EP03 : Babel and JSX

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", {id : "parent"},
-    [   React.createElement("div",{id : "child1"},
-        [   React.createElement("h1",{},"I am H1"),
-            React.createElement("h2",{},"I am H2")
-        ]
-        ),
-        React.createElement("div",{id : "child2"},
-        [   React.createElement("h3",{},"I am H3"),
-            React.createElement("h4",{},"I am H4")
-        ]
-        )
-    
-    ]
-   
-)
-console.log(parent)
 
-const heading = React.createElement("h1",
-    {id : "heading", className : "headingClass"},
-    "Hello World from React!");
-// console.log(heading) // object
+// React.createElement => React Element - JSObject => HTML Element(render())
+
+const heading = React.createElement("h1",{id : "heading"}, "Namaste React ⚛️");
+
+// JSX - HTML Like or XML Like syntax
+// transpiled before it reaches the JS Engine -> Parcel -> Babel
+// JSX(Babel) => React.createElement => React Element - JSObject => HTML Element(render())
+const jsxHeading = <h1 id = "heading">Namaste React using JSX ⚛️</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+root.render(jsxHeading);
