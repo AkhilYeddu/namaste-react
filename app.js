8f12f89e0eea78cc5436713f162ed2343f05c1cc
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-07 03:16:26 +0530

    EP03 : different ways of using a component

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Title =()=> (
+    <h1 className="header">
+        Namaste React using JSX 🚀
+    </h1>
+)
 
 
 // Functional Component
-
 const HeadingComponent = ()=>{
     return (
         <div id= "container">
             <Title/>
-            
+            <Title></Title> 
+            {Title()}
             <h2 id="chinno">{console.log("hello") }</h2>
             <h1 className="heading">Namaste Functional Component ⚛️</h1>
         </div>
     )
 }
 
-// Another way to write function component
-const Title = ()=> (
-    <h1 className="header">Namaste React using JSX 🚀
-    </h1>
-    
-)
-
-
-const element2 = (
-    <span>Namaste React Element 2 ⚛️</span>
-)
-
-// React Element
-const element = (
-    <h1>
-    {element2}
-        Namaste React Element 🚀
-        <HeadingComponent/>
-    </h1>
-   
-)
 
 
 
@@ -49,6 +33,7 @@ const number = 26;
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<HeadingComponent/>);
+
 
 
