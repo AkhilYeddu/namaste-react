b8f1040af3d347b254ef2d2eac541b8c6cb2738f
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-07 02:45:43 +0530

    EP03: element in element, component in element

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Another way to write function component
-const Title = ()=> (
-    <h1 className="header">Namaste React using JSX 🚀</h1>
-)
 
-
-// React Element
-const element = (
-    <h2>Namaste React Element 🚀</h2>
-)
-
-const number = 26;
 
 // Functional Component
 
@@ -20,16 +9,39 @@ const HeadingComponent = ()=>{
     return (
         <div id= "container">
             <Title/>
-            {element}
+            
             <h2 id="chinno">{console.log("hello") }</h2>
             <h1 className="heading">Namaste Functional Component ⚛️</h1>
         </div>
     )
 }
 
+// Another way to write function component
+const Title = ()=> (
+    <h1 className="header">Namaste React using JSX 🚀
+    </h1>
+    
+)
+
+
+const element2 = (
+    <span>Namaste React Element 2 ⚛️</span>
+)
+
+// React Element
+const element = (
+    <h1>
+    {element2}
+        Namaste React Element 🚀
+        <HeadingComponent/>
+    </h1>
+   
+)
 
 
 
+
+const number = 26;
 
 
 
@@ -37,6 +49,6 @@ const HeadingComponent = ()=>{
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(element);
 
 
