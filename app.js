cca63c5cdd890a51283b513b4eb0d54f0bbbb82a
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-07 01:53:01 +0530

    EP03: Functional Components

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => React Element - JSObject => HTML Element(render())
+// React Element
+const jsxHeading = (<h1 id = "heading">Namaste React using JSX ⚛️</h1>)
 
-const heading = React.createElement("h1",{id : "heading"}, "Namaste React ⚛️");
 
-// JSX - HTML Like or XML Like syntax
-// transpiled before it reaches the JS Engine -> Parcel -> Babel
-// JSX(Babel) => React.createElement => React Element - JSObject => HTML Element(render())
-const jsxHeading = <h1 id = "heading">Namaste React using JSX ⚛️</h1>
+// Functional Component
+
+const HeadingComponent = ()=>{
+    return <h1 className="heading">Namaste Functional Component</h1>
+}
+// BOTH ARE SAME
+const HeadingComponent2 = ()=> (
+    <h1 className="heading">Namaste Functional Component</h1>
+);
+
+
+
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
+
+
