fc78c54d6eb68f10b1156954f13f36f0c12dc664
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-08 00:52:32 +0530

    EP04: cleaned our code, more object destructuring, removed height of the card for auto adjustment

--- app.js
+++ app.js
@@ -39,17 +39,17 @@ const Header = ()=>{
 
 const RestaurantCard = (props)=>{
     const{ resData } = props; // resData is inside props.
-    console.log(resData)
+    const {name, cuisines, avgRating, costForTwo, deliveryTime, cloudinaryImageId} = resData?.data; //optional chaining
     return(
         <div className="res-card">
-            <img className="res-logo" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + resData.data.cloudinaryImageId}>
+            <img className="res-logo" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + 
+                cloudinaryImageId}>
             </img>
-        
-            <h3>{resData.data.name}</h3>
-            <h4>{resData.data.cuisines.join(", ")}</h4>
-            <h4>{resData.data.avgRating} stars</h4>
-            <h4>₹ {resData.data.costForTwo / 100} for Two</h4>
-            <h4>{resData.data.deliveryTime} minutes</h4>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>₹ {costForTwo / 100} for Two</h4>
+            <h4>{deliveryTime} minutes</h4>
         </div>
     )
 }
@@ -1948,6 +1948,23 @@ const Body = ()=>{
 
             <div className="res-container">
                 <RestaurantCard resData = {resList[0]}/>
+                <RestaurantCard resData = {resList[1]}/>
+                <RestaurantCard resData = {resList[2]}/>
+                <RestaurantCard resData = {resList[3]}/>
+                <RestaurantCard resData = {resList[4]}/>
+                <RestaurantCard resData = {resList[5]}/>
+                <RestaurantCard resData = {resList[6]}/>
+                <RestaurantCard resData = {resList[7]}/>
+                <RestaurantCard resData = {resList[8]}/>
+                <RestaurantCard resData = {resList[9]}/>
+                <RestaurantCard resData = {resList[10]}/>
+                <RestaurantCard resData = {resList[11]}/>
+                <RestaurantCard resData = {resList[12]}/>
+                <RestaurantCard resData = {resList[13]}/>
+                <RestaurantCard resData = {resList[14]}/>
+
+
+
                
             </div>
         </div>
