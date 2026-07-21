d6b34bd590236da116ab9a8e59ac901da3be17a8
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-21 18:19:08 +0530

    EP07: using Outlet to render Child components (children elements)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import RestaurantCard from "./components/RestaurantCard";
-import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Route, RouterProvider, Outlet} from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -13,12 +13,7 @@ const AppLayout = ()=>{
     return(
         <div className="app">
         <Header/>
-        {/* {if path is / then it should render} */}
-        <Body/>
-        {/* {if path is /about then it should render} */}
-        <About/>
-        {/* {if path is /contact then it should render} */}
-        <Contact/>
+        <Outlet/>
         </div>
     )
 }
