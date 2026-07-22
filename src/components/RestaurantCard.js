8b1bd70e115b5c7700936f7da9855ed823b1116e
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-23 00:55:52 +0530

    EP07: fixed costForTwo Bug

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -11,7 +11,7 @@ const RestaurantCard = (props)=>{
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} stars</h4>
-            <h4>{costForTwoString}</h4>
+            <h4>{costForTwo}</h4>
             <h4>{sla.deliveryTime} mins</h4>
         </div>
     )
