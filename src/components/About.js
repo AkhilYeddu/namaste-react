eb7d7c554b2fca6520d6f1217fef6c502928b06e
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-23 19:04:58 +0530

    User (fn component) in About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
+import User from "./User"
+
 const About = ()=>{
     return(
         <div>
             <h1>This is About page</h1>
             <h2>this is some information about this About Page</h2>
+            <User/>
         </div>
     )
 }
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = ()=>{
+    return(
+        <div className="user-card">
+                <h3>Name : Akhil</h3>
+                <h3>City : Visakhapatnam</h3>
+                <h3>Contact : 8919262972</h3>
+        </div>
+    )
+}
+
+export default User
