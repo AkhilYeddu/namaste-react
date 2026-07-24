08342718fbd9515be91bd7e4f665a20a026df24a
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-24 19:01:24 +0530

    EP08 : intro to setState() how to use it

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,6 @@ const About = ()=>{
         <div> 
             <h1>This is About page</h1>
             <h2>this is some information about this About Page</h2>
-            <User name= {"Akhil (function)"}/>
-
             <UserClass name={"Akhil (class)"} location={"Visakhapatnam (class)"} contact = {"8919562972 (class)"}/>
         </div>
     )
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,18 +6,23 @@ class UserClass extends React.Component{
             console.log(props)
 
             this.state = {
-                count : 0,
-                count1 : 2
+                count : 0
             }
         }
  
     render(){
         const {name, location, contact} = this.props;
-        const {count, count1} = this.state;
+        const {count} = this.state;
         return(
             <div className="user-card">
                 <h2>Count = {count}</h2>
-                <h2>Count1 = {count1}</h2>
+                <button onClick={()=>{
+                    this.setState({
+                        count : this.state.count + 1
+                    })
+                }}>
+                    Click me
+                </button>
                 <h3>Name : {name}</h3>
                 <h3>City : {location}</h3>
                 <h3>Contact : {contact}</h3>
