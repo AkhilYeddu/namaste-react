12910955be14c8052bfe3c282644f934db4434c4
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-25 01:23:18 +0530

    EP08 : live user api from github, notes of the lifecycle

--- src/components/About.js
+++ src/components/About.js
@@ -3,20 +3,19 @@ import React from "react"
 class About extends React.Component{
     constructor(props){
         super(props)
-        console.log("Parent constructor")
+        // console.log("Parent constructor")
     }
     componentDidMount(){
-        console.log("Parent component did mount")
+        // console.log("Parent component did mount")
     }
 
     render(){
-        console.log("Parent render")
+        // console.log("Parent render")
         return(
         <div> 
             <h1>This is About page</h1>
             <h2>this is some information about this About Page</h2>
             <UserClass name={"First"} location={"Visakhapatnam (class)"} contact = {"8919562972 (class)"}/>
-            <UserClass name={"Second"} location={"Hyderabad (class)"} contact = {"7989406639 (class)"}/>
         </div>
         )
     }
@@ -24,25 +23,3 @@ class About extends React.Component{
 }
 
 export default About
-
-/*
-
-- Parent constructor
-- Parent render
-    - first constructor
-    - first render
-
-    - second constructor
-    - second render
-
-    <DOM UPDATED - IN A SINGLE BATCH>
-
-    - first component did mount
-    - second component did mount 
-- Parent component did mount 
-
-
-
-
-
-*/
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,33 +6,63 @@ class UserClass extends React.Component{
             console.log(this.props.name + " constructor")
 
             this.state = {
-                count : 0
+                userInfo:{
+                    name : "dummy name",
+                    location : "dummy location"
+                }
             }
         }
-    componentDidMount(){
+
+    async componentDidMount(){
+       
         console.log(this.props.name + " component did mount")
-        // Api calls are made here
+        const data = await fetch("https://api.github.com/users/AkhilYeddu");
+        const json = await data.json();
+        
+        this.setState({
+            userInfo : json
+        })
+
+        
+        
+       
     }
+
     render(){
-        const {name, location, contact} = this.props;
-        const {count} = this.state;
         console.log(this.props.name + " render")
+        
+        const{name, location, avatar_url} = this.state.userInfo
+        
+        
         return(
             <div className="user-card">
-                <h2>Count = {count}</h2>
-                <button onClick={()=>{
-                    this.setState({
-                        count : this.state.count + 1
-                    })
-                }}>
-                    Click me
-                </button>
+                <img src={avatar_url}></img>
                 <h3>Name : {name}</h3>
                 <h3>City : {location}</h3>
-                <h3>Contact : {contact}</h3>
+                
         </div>
         )
     }
 }
 
 export default UserClass
+
+/*
+
+------- MOUNTING --------
+
+constructor(dummy)
+render(dummy)
+    <HTML dummy>
+component did mount
+    <API Call>
+    <this.setState> -> state variable changes, triggers reconcialtion 
+
+------- UPDATE ------- 
+render(api data)
+    <HTML new api data>
+component did update
+
+
+
+*/
