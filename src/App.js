a878517ad8232b5250be3c7d9eac8d6737cb0a13
AkhilYeddu <akhilkiu10@gmail.com>
2026-08-22 16:00:56 +0530

    EP09 : LazyLoading use case

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -8,7 +8,9 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+// import Grocery from "./components/Grocery";
 
+const Grocery = lazy(()=> import("./components/Grocery"))
 
 const AppLayout = ()=>{
     return(
@@ -40,6 +42,10 @@ const appRouter = createBrowserRouter([
             {
                 path:"/restaurants/:resId",
                 element: <RestaurantMenu/>
+            },
+            {
+                path : "/grocery",
+                element: <Grocery/>
             }
 
         ]
---
+++ src/components/Grocery.js
@@ -0,0 +1,7 @@
+const Grocery = ()=>{
+    return(
+        <h1>This is Grocery page! lot of smaller components are present here!!</h1>
+    )
+}
+
+export default Grocery
