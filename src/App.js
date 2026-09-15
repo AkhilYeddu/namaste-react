5d37828bc40359e93eab415742187247f203c323
AkhilYeddu <akhilkiu10@gmail.com>
2026-09-15 14:30:05 +0530

    EP12 : Providing our store to the app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 // import Grocery from "./components/Grocery";
 
 
@@ -36,12 +38,15 @@ const AppLayout = ()=>{
     
 
     return(
-        <UserContext.Provider value={{loggedInUser : userName, setUserName}}>
+        <Provider store= {appStore}>
+            <UserContext.Provider value={{loggedInUser : userName, setUserName}}>
                 <div className="app">
                 <Header/>
                 <Outlet/>
                 </div>
-        </UserContext.Provider>
+            </UserContext.Provider>
+        </Provider>
+        
         
     )
 }
